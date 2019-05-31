3b0e4c030fcb98eaf0004bae5bc0e5ecdd89c50d
alex-kudryashov <50145588+alex-kudryashov@users.noreply.github.com>
2019-05-31 20:10:30 +0300

    51

--- slider+animation/js/slider.js
+++ slider+animation/js/slider.js
@@ -1,13 +1,20 @@
 var left = document.getElementById('slider-left');
-var right = document.getElementById('slider-right');
 var left=0;
+var timer;
+autoSlider();
+function autoSlider () {
+	timer = setTimeout(sliderLeft, 6000);
+}
+
 function sliderLeft() {
 	var polosa = document.getElementById('line');
 	left = left - 300;
 	if (left<-900) {
 		left = 0;
+		clearTimeout(timer);
 	};
 	polosa.style.left = left+'px';
+	autoSlider();
 }
 
 function sliderRight () {
