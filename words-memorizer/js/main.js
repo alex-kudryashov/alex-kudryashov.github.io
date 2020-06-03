fc7d8399a13daae9df4002dc5d731d74fff86892
alex-kudryashov <kudryashov_2017@bk.ru>
2020-06-03 15:49:06 +0300

    -

--- words-memorizer/js/main.js
+++ words-memorizer/js/main.js
@@ -270,7 +270,8 @@ $forgotBtn.on('click', () => {
       word.forgotInThisSession = true;
     }
     if (word.wordId == currentWord && word.learnedCount == -1) {
-      word.learnedCount = '1';
+      word.learnedCount = '0';
+      word.date = now.setMinutes(now.getMinutes() + 5);
     }
   })
   nextWord();
